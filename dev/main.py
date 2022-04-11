68ec526f7161bdfc2735e66e3b66c19fc5a02cfa
Vicemc <victormclimaco@gmail.com>
2022-04-11 15:53:59 -0300

    Protótipo de CRUD, conexão a fazer

--- dev/main.py
+++ dev/main.py
@@ -1,1 +1,30 @@
-print("Hello World!")
+import mysql.connector
+import datetime
+
+
+connection = mysql.connector.connect(
+    host="localhost",
+    user="user",
+    password="1234",
+    database="bancodados"
+)
+
+cursor = connection.cursor()
+
+sql = ""
+data = (
+    'Primeiro Usuário',
+    'primeirousuário@teste.com',
+    datetime.datetime.today()
+)
+
+cursor.execute(sql, data)
+connection.commit()
+
+userid = cursor.lastrowid
+
+cursor.close()
+connection.close()
+
+print("Novo usuário: ", userid)
+
