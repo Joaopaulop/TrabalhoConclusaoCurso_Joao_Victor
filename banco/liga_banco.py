067b04c234788099a58249d622114038b34da7b8
Joao Paulo <joaopauloe.paiva@gmail.com>
2022-04-26 16:41:24 -0300

    select

--- banco/liga_banco.py
+++ banco/liga_banco.py
@@ -2,3 +2,11 @@ import sqlite3
 
 conn = sqlite3.connect('bd_tcc.db')
 
+def selecao_nome_usuario(conn, nome):
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM TCC.USUARIO WHERE NOME=?",(nome,))
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
