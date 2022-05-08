6ef520d0095e59bed70f58f9b9d73471c39724e4
Joao Paulo <joaopauloe.paiva@gmail.com>
2022-05-08 16:26:44 -0300

    teste main

--- view/main.py
+++ view/main.py
@@ -1,9 +1,24 @@
 from kivy.app import App
 from kivy.uix.button import Button
-from matplotlib.pyplot import text
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+
 
 class Test(App):
     def build(self):
-        return Button(text="hello world \n  Sucesso no TCC")
+        box = BoxLayout(orientation='vertical')
+        button = Button(text='Botao 1')
+        label = Label('Texto 1')
+        box.add_widget(button)
+        box.add_widget(label)
+
+        box2 = BoxLayout()
+        button2 = Button(text='Botao 2')
+        label2 = Label(text='texto 2')
+        box2.add_widget(button2)
+        box2.add_widget(label2)
+
+        box.add_widget(box2)
+        return box
         
 Test().run()
